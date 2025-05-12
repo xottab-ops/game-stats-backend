6998f77fb5ddc9d8183247114fc58c35cf86eca1
XoTTaB <jassik2002@mail.ru>
2025-05-13 00:11:20 +1000

    FEAT: Add healthcheck route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,9 @@
 from .games_routes import games_bp
 from .games_aggregate_routes import game_stats_bp
+from .healthcheck_route import healthcheck_bp
 
 
 def register_blueprints(app):
     app.register_blueprint(games_bp)
     app.register_blueprint(game_stats_bp)
+    app.register_blueprint(healthcheck_bp)
